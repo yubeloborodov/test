abf30382389d13c20b6fdd27f286e5dde403c4ac
yubeloborodov <yury.beloborodov@gmail.com>
2022-12-11 21:20:11 +0300

    Add 4

--- Lesson 2/4.rb
+++ Lesson 2/4.rb
@@ -1,1 +1,12 @@
 # Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
+vowel_letters = %w[а у о и э ы я ю е ё]
+alphabet = ('а'..'я')
+hash = {}
+
+alphabet.each_with_index do |alphabet_letter, index|
+  vowel_letters.each do |vowel_letter|
+    hash[vowel_letter] = index if vowel_letter == alphabet_letter
+  end
+end
+
+puts hash
