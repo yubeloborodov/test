6f53d69129dc1bd4f2a38328061bb277219a1430
yubeloborodov <yury.beloborodov@gmail.com>
2022-12-12 01:36:33 +0300

    Add 5 task

--- Lesson 2/5.rb
+++ Lesson 2/5.rb
@@ -3,6 +3,8 @@
 months = [{ 'january' => 31 }, { 'february' => 28 }, { 'march' => 31 }, { 'april' => 30 }, { 'may' => 31 }, { 'june' => 30 }, { 'july' => 31 }, { 'august' => 31 },
           { 'september' => 30 }, { 'october' => 31 }, { 'november' => 30 }, { 'december' => 31 }]
 
+sum = 0
+
 puts 'Введите номер дня:'
 d = gets.chomp.to_i - 1
 
